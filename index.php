0c772f2ba525a1dc68e41abe9e64f30e4c4fcdfc
sjoshi-1 <200455953@student.georgianc.on.ca>
2023-04-27 19:46:34 -0400

    add images

--- index.php
+++ index.php
@@ -3,17 +3,61 @@
 include("header.php");
 ?>
 
-<div class="container">
+<div class="container mt-5">
     <div class="row">
-        <div class="col-lg-3">
-        <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div class="col-lg-6">
+            <!--to save data-->
+            <form>
+        <div class="card">
+  <img src="images/shoes1.jpg" class="card-img-top" alt="...">
+  <div class="card-body text-center">
+    <h5 class="card-title">Air jordan</h5>
+    <p class="card-text">Price:$225</p>
+    <button type="submit" class="btn btn-info">Add To Cart</button>
+    <input type="hidden" name="Item_Name" value="Shoe-1">
+    <input type="hidden" name="Price" value="$225">
   </div>
 </div>
+</form>
         </div>
     </div>
+    <!--item2-->
+    <div class="col-lg-6">
+            <!--to save data-->
+            <form>
+        <div class="card">
+  <img src="images/shoes2.jpg" class="card-img-top" alt="...">
+  <div class="card-body text-center">
+    <h5 class="card-title">Air jordan</h5>
+    <p class="card-text">Price:$225</p>
+    <button type="submit" class="btn btn-info">Add To Cart</button>
+    <input type="hidden" name="Item_Name" value="Shoe-1">
+    <input type="hidden" name="Price" value="$225">
+  </div>
+</div>
+</form>
+        </div>
+    </div>
+   
+    <!--item3-->
+    
+    <div class="col-lg-6">
+            <!--to save data-->
+            <form>
+        <div class="card">
+  <img src="images/shoes3.jpg" class="card-img-top" alt="...">
+  <div class="card-body text-center">
+    <h5 class="card-title">Air jordan</h5>
+    <p class="card-text">Price:$225</p>
+    <button type="submit" class="btn btn-info">Add To Cart</button>
+    <input type="hidden" name="Item_Name" value="Shoe-1">
+    <input type="hidden" name="Price" value="$225">
+  </div>
+</div>
+</form>
+        </div>
+    </div>
+
+
+</div>
 </div>
