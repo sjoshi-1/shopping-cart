67a535e86e5b92f585329764149d15e0b0915820
sjoshi-1 <200455953@student.georgianc.on.ca>
2023-04-27 19:23:59 -0400

    navbar

---
+++ header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en-CA">
+    <head>
+        <meta charset="utf-8">
+        <title>Shopping cart</title>
+        <meta name="description" content="shoes">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        
+        <!--bootstrap-->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+ 
+        <!--fonts-->
+        <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@1,300&display=swap" rel="stylesheet">
+
+        <!--css-->
+        <link href="style.css" rel="stylesheet">
+ </head>
+    <body>
+        <!--Navbar-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <!--this class helps to keep search on right corner--> 
+    <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link"  href="index.php">Home </a>
+        </li>
+</ul>
+        
+      <div>
+        <a href="mycart.php" class="btn btn-outline-success">My Cart (0)</a>
+</div>
+    
+  </div>
+</nav>
+    </body>
+</html>
